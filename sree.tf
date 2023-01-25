96cc4d793cc4106041406890154b5581816ab6f2
SREEKANTH A <110668483+SREEKANTH1313@users.noreply.github.com>
2023-01-25 10:13:40 +0530

    Update sree.tf

--- sree.tf
+++ sree.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "server" {
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
   subnet_id="subnet-0f5e1db05acd752b2 "
-  security_groups=["sg-05b3f1a6fd83be07c "]
+  security_groups=["sg-0ad5ff42b16ca530b"]
   
 
 
